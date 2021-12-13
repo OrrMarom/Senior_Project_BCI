cbd16a5d2b650c3cba9e42cbbed86c965a46b190
OrrMarom <itsorrmarom@gmail.com>
2021-12-12 23:11:18 -0600

    detect open/closed eyes using band power

--- parse_bands.py
+++ parse_bands.py
@@ -47,45 +47,114 @@ def main():
     board.start_stream(45000, args.streamer_params)
     time.sleep(5)
     # data = board.get_current_board_data (256) # get latest 256 packages or less, doesnt remove them from internal buffer
-    data = board.get_board_data()  # get all data and remove it from internal buffer
+    # get all data and remove it from internal buffer
     # board.stop_stream()
     # board.release_session()
 
-    print("\n\n\n\n\n\n")
-    for i in range(10):
-        list_data = data.tolist()
-        _get_wave_sections(list_data[0])
-        time.sleep(1)
+    print("\n\n\n\n\n\n\n\n\n")
+    refresh_rate=1 #in seconds
+    for i in range(1000):
+        data = board.get_board_data()
+        frequencies = data.tolist()[2]
+        band_counts = _average_waves_over_time(frequencies)
+        _get_wave_sections(band_counts)
+        time.sleep(refresh_rate)
+
 
     board.stop_stream()
     board.release_session()
 
-def _get_wave_sections(frequencies):
-    alpha,beta,delta,theta,other=0,0,0,0,0
-    total_num = len(frequencies)
+def _average_waves_over_time(frequencies):
+    alpha,beta,delta,theta,gamma,other,total=0,0,0,0,0,0,0
+    
     for num in frequencies:
-        if(num>=1 and num<=5):
+        total+=1
+
+        if(num>1 and num<=4):
             delta+=1
-        elif(num>=4 and num<=8):
+        elif(num>4 and num<=8):
             theta+=1
-        elif(num>=8 and num<=13):
+        elif(num>8 and num<=13):
             alpha+=1
-        elif(num>=13 and num<=30):
+        elif(num>13 and num<=30):
             beta+=1
+        elif(num>30 and num<=45):
+            gamma+=1
         else:
             other+=1
+            
 
-    goback = "\033[F" * 8
+    final_obj = {
+        "alpha":alpha,
+        "beta":beta,
+        "delta":delta,
+        "theta":theta,
+        "gamma":gamma,
+        "other":other,
+        "total":total
+    }
+    return final_obj
+
+def _get_wave_sections(band_counts):
+    # Determine ratios of frequencies
+    delta=band_counts["delta"]
+    theta=band_counts["theta"]
+    alpha=band_counts["alpha"]
+    beta=band_counts["beta"]
+    gamma=band_counts["gamma"]
+    other=band_counts["other"]
+
+
+    # Convert to percentages
+    total_num_waves = band_counts["total"]
+    adjusted_num = total_num_waves - other if not other==total_num_waves else 1
+
+    delta_band=round(delta/adjusted_num*100,2)
+    theta_band=round(theta/adjusted_num*100,2)
+    alpha_band=round(alpha/adjusted_num*100,2)
+    beta_band=round(beta/adjusted_num*100,2)
+    gamma_band=round(gamma/adjusted_num*100,2)
+
+    # Print the bands table
+    blink = False
+    blink = _check_for_closed_eyes(alpha_band,beta_band,delta_band,theta_band,gamma_band)
+    if(blink):
+        goback = "\033[F" * 12
+    else:
+        goback = "\033[F" * 10
+
+    goback = "\033[F" * 10
     result = f"""{goback}
     Bands
     ----------
-    Alpha: {alpha/total_num*100}
-    Beta: {beta/total_num*100}
-    Delta: {delta/total_num*100}
-    Theta: {theta/total_num*100}
-    Other: {other/total_num*100}"""
+    Delta[1-4]: {delta_band}%     
+    Theta[4-8]: {theta_band}%     
+    Alpha[8-13]: {alpha_band}%     
+    Beta[13-30]: {beta_band}%     
+    Gamma[30-45]: {gamma_band}%     
+
+    """
+    if(blink):
+        result+="*Eyes are closed*"
+    else:
+        result+="                  "
+
+
+
     print(result)
     sys.stdout.flush()
+    
+def _get_percentage(band,total_num):
+    return round(band*100/total_num,2)
+    
+
+def _check_for_closed_eyes(alpha,beta,delta,theta,gamma):
+    if(
+        alpha>=15
+    ):
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
     main()
