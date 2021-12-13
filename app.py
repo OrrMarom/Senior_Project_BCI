e65c2d9881055cb76699e25c63dd0e7eabc3ac9b
jagui1696 <joshua.aguilar.128@my.csun.edu>
2021-12-12 18:53:59 -0800

    added frames in streaming frame
    1. frame for graph space
    2. frame for shortcut space

--- app.py
+++ app.py
@@ -18,6 +18,15 @@ frame_buttons.pack(side=tk.LEFT, fill="both")
 frame_streaming = tk.Frame(root, width=900, height=800, bg="#3474A8")
 frame_streaming.pack(side=tk.LEFT, fill="both")
 
+frame_s_graph = tk.Frame(frame_streaming, width=800, height=300, bg="green")
+frame_s_graph.pack(pady=30, padx=30)
+
+frame_s_short = tk.Frame(frame_streaming, width=800, height=300, bg="blue")
+frame_s_short.pack(pady=30, padx=30)
+
+#frame_s_short_title = tk.Frame(frame_s_short, width=800, height=50, bg="white")
+#frame_s_short_title.pack()
+
 button_connect = tk.Button(frame_buttons, text="Connect Headset", width=25, height=5, padx=10, pady=5, fg="white", bg="#28817C",
     font=(18))
 button_connect.pack()
