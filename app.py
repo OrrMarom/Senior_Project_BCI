6451dddc6617666c6c903ec447151b155e4c699e
tonyvillicana <78631526+tonyvillicana@users.noreply.github.com>
2021-12-13 12:51:21 -0800

    Update app.py
    New Commit

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ frame_s_short.pack(pady=30, padx=30)
 
 #frame_s_short_title = tk.Frame(frame_s_short, width=800, height=50, bg="white")
 #frame_s_short_title.pack()
+#frame_s_short_title.pack()
 
 button_connect = tk.Button(frame_buttons, text="Connect Headset", width=25, height=5, padx=10, pady=5, fg="white", bg="#28817C",
     font=(18))
