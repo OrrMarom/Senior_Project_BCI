5074b15d353ba5c6223c9bd3234a6829a2ca6583
OrrMarom <itsorrmarom@gmail.com>
2022-05-08 20:46:38 -0500

    polishing stuff

--- bigbrain_app.py
+++ bigbrain_app.py
@@ -1,10 +1,11 @@
+from cProfile import label
 import sys
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtGui
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import *
 from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds
-from brainflow.data_filter import DataFilter, FilterTypes, DetrendOperations, WindowFunctions, DetrendOperations
+from brainflow.data_filter import DataFilter, FilterTypes, DetrendOperations, WindowFunctions, DetrendOperations, NoiseTypes, AggOperations
 import argparse
 import logging
 import numpy as np
@@ -14,6 +15,7 @@ import time
 import webbrowser 
 import pyautogui
 from PyQt5.QtGui import QIntValidator
+import json
 
 class MainAppWindow(QWidget):
     def __init__(self):
@@ -199,6 +201,18 @@ class MainAppWindow(QWidget):
         boardshim = boardshim_init_results["boardshim"]
         args = boardshim_init_results["args"]
 
+        # initialize blink stuff
+        self.blink_count = 0
+        self.blink_cooldown_counter = 0
+        self.jawClench = False
+        self.eyeBlink = False
+        self.jaw_stop_counter = 0
+        self.jaw_stop_limit = 0
+        self.print_file = []
+        self.click_safety=False
+        #! how long pyautogui pauses between every function
+        pyautogui.PAUSE = 0.005
+
         # initiating time series and fields for ts
         self.board_id = board_id #board_shim.get_board_id()
         self.boardshim = boardshim
@@ -213,7 +227,9 @@ class MainAppWindow(QWidget):
         #* Start the brainwave data streaming in the background
         self._init_timeseries(self.graphWidget)
         self.graphTimer = QtCore.QTimer()
-        self.graphTimer.setInterval(50)
+
+        self.loop_interval = 1 # in milliseconds
+        self.graphTimer.setInterval(self.loop_interval)
         self.graphTimer.timeout.connect(self._update_brainwave_data)
         self.graphTimer.start()
 
@@ -232,7 +248,10 @@ class MainAppWindow(QWidget):
         self.plots = list()
         self.curves = list()
         for i in range(len(self.exg_channels)):
+            graphWidget
             p = graphWidget.addPlot(row=i,col=0)
+            p.setYRange(-200,200,padding=0)
+
             # p.showAxis('left', True)
             # p.setMenuEnabled('left', False)
             # p.showAxis('bottom', False)
@@ -250,6 +269,7 @@ class MainAppWindow(QWidget):
                 curve = p.plot(pen = {"color":"#5ED02F"})
             else:
                 curve = p.plot()
+
             self.curves.append(curve)
             
     # Updates brainwave data over time
@@ -266,22 +286,37 @@ class MainAppWindow(QWidget):
                                         FilterTypes.BUTTERWORTH.value, 0)
             DataFilter.perform_bandstop(self.data[channel], self.sampling_rate, 60.0, 4.0, 2,
                                         FilterTypes.BUTTERWORTH.value, 0)
+
+            # DataFilter.perform_rolling_filter(self.data[channel], 3, AggOperations.MEDIAN.value)
+
+            DataFilter.remove_environmental_noise (self.data[channel],self.sampling_rate, 1)
+
+
             self.curves[count].setData(self.data[channel].tolist())
 
             # Calculate data for cursor control to use later
             if(channel==1):
                 nfft = DataFilter.get_nearest_power_of_two(self.sampling_rate)
+                # psd = DataFilter.get_psd_welch(self.data[channel], nfft, nfft // 2, self.sampling_rate, WindowFunctions.BLACKMAN_HARRIS.value)
                 psd = DataFilter.get_psd_welch(self.data[channel], nfft, nfft // 2, self.sampling_rate, WindowFunctions.BLACKMAN_HARRIS.value)
-                
+
+                frequency= self.data[channel].tolist().pop()
+
                 delta_band = DataFilter.get_band_power(psd, 1.0, 3.0)
                 theta_band = DataFilter.get_band_power(psd, 4.0, 8.0)
                 alpha_band = DataFilter.get_band_power(psd, 8.0, 13.0)
                 beta_band = DataFilter.get_band_power(psd, 13.0, 30.0)
                 gamma_band = DataFilter.get_band_power(psd, 30.0, 45.0)
-                print([delta_band,gamma_band])
+            
 
+                print_thing = str(["frequency:",int(frequency),"bandstuff:",int(delta_band),int(theta_band),int(alpha_band),int(beta_band),int(gamma_band),"eyeblinkstuff:", self.blink_count,self.blink_cooldown_counter,self.jaw_stop_counter,self.jawClench,self.eyeBlink])
+                self.print_file.append(print_thing)
+                # print(print_thing)
+
+                self.blink_cooldown_counter += self.loop_interval
+                self.jaw_stop_counter += self.loop_interval
                 self.jawClench = checkJawClenching(gamma_band)
-                self.eyeBlink = checkEyeBlinking(delta_band,gamma_band)
+                self.eyeBlink = checkEyeBlinking(frequency)
     
     #*---------------------
     #* Start Cursor Control Helper
@@ -296,23 +331,80 @@ class MainAppWindow(QWidget):
 
         # Start cursor control
         self.cursorTimer = QtCore.QTimer()
-        self.cursorTimer.setInterval(50)
-        self.cursorTimer.timeout.connect(self._update_cursor_movement)
+        self.cursorTimer.setInterval(self.loop_interval)
+        
+        #? self.cursorTimer.timeout.connect(self._update_1d_cursor_movement)
+        
+        # 2d control
+        self.cooldown_rate= 300 # in milliseconds
+        self.blink_count = 0 #indicates direction
+        self.blink_cooldown_counter = self.cooldown_rate #counts how long it's been since last blink
+        self.blink_cooldown_limit = self.blink_cooldown_counter #how long to wait before registering another blink_count in milliseconds
+
+        self.jaw_stop_counter = 100 # Force jaw to be active for this many milliseconds before being able to disable
+        self.jaw_stop_limit = 100
+        self.jaw_clench_start = False
+
+        self.cursorTimer.timeout.connect(self._update_2d_cursor_movement)
+
         #! Comment out line below to prevent cursor movement
         self.cursorTimer.start()
 
-    def _update_cursor_movement(self):
+    # Directly works on 1d left-right movement
+    def _update_1d_cursor_movement(self):
         if self.jawClench:
             testMoveCursorLeft()
         elif self.eyeBlink:  
             testMoveCursorRight()
+
+    # process for 2d cursor movement
+    def _update_2d_cursor_movement(self):
+        if self.eyeBlink and not self.jaw_clench_start:
+            if self.blink_cooldown_counter > self.blink_cooldown_limit:
+                self.blink_count += 1
+                self.blink_cooldown_counter = 0
+
+            if self.blink_count>4:
+                self.blink_count = 0
+        elif self.jawClench:  
+            self.jaw_clench_start = True
+            self.move2DCursor()
+
+        if self.jaw_clench_start and not self.jawClench:
+            # if(self.jaw_stop_counter>self.jaw_stop_limit):
+            self.jaw_clench_start = False
+            self.click_safety = False
+            # self.blink_count = 0
+            self.jaw_stop_counter = 0
+
+    def move2DCursor(self):
+        direction = self.blink_count
+        movement_speed = 1
+        duration = 0.01
+
+        if direction == 0 and not self.click_safety: #click
+            pyautogui.click()
+            self.click_safety = True
+        elif direction==1: #left
+            pyautogui.move(-1 * movement_speed, 0, duration=duration)
+        elif direction ==2: #up
+            pyautogui.move(0, -1 * movement_speed, duration=duration)
+        elif direction ==3: #right
+            pyautogui.move(movement_speed,0, duration=duration)
+        elif direction ==4: #down
+            pyautogui.move(0,movement_speed, duration=duration)
 
     #*---------------------
     #* Start Save Helper
     #*---------------------
     def saveData(self):
         dataHeader = "{}{}".format(self.userName, self.date)
-        DataFilter.write_file(self.data, '{}'.format(dataHeader), 'a') # 'a' appends to file, 'w' for overwrite
+        # DataFilter.write_file(self.data, '{}'.format(dataHeader), 'a') # 'a' appends to file, 'w' for overwrite
+
+        f = open("test.txt", "w")
+        f.write(json.dumps(self.print_file,indent=4))
+        f.close()
+
         print("saved local file")
 
     #*---------------------
@@ -461,7 +553,34 @@ class CursorAppWindow(QWidget):
         # self.pushButton.setFlat(True)
         self.pushButton.setStyleSheet("font-weight: bold; color: white; background-color: #D20705;")
         self.pushButton.clicked.connect(lambda: self.goback(parent))
+        self.directionLabel = QLabel("", self)
+        self.directionLabel.setStyleSheet("font-weight: bold; color: white; background-color: #00db33; padding: 3px;")
+    
+        print('hi')
+        self.label_timer = QtCore.QTimer()
+        self.loop_interval = 100 # in milliseconds
+        self.label_timer.setInterval(self.loop_interval)
+        self.label_timer.timeout.connect(lambda: self._change_direction_text(parent))
+        self.label_timer.start()
         self.center() 
+
+    def _change_direction_text(self,parent):
+        blink_count = parent.blink_count
+        print(blink_count)
+        label_val = ""
+        if blink_count == 0:
+            label_val= "Click"
+        elif blink_count == 1:
+            label_val= "Left"
+        elif blink_count == 2:
+            label_val= "Up"
+        elif blink_count == 3:
+            label_val= "Right"
+        elif blink_count == 4:
+            label_val= "Down"
+
+        self.directionLabel.setText(label_val)
+
 
     def goback(self, parent):
         # stop the loop for checking cursor movement
@@ -529,12 +648,12 @@ def init_boardshim_item():
 #* Start Cursor Connection stuff
 #*---------------------
 def checkJawClenching(gamma_band):
-    if gamma_band > 20:
+    if gamma_band > 152:
         return True
     else:
         return False
-def checkEyeBlinking(delta_band, gamma_band):
-    if delta_band > 80 and gamma_band < 5:
+def checkEyeBlinking(frequency):
+    if frequency < -140:
         return True
     else:
         return False
